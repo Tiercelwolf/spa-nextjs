4ea997622c260205d9a2096968cec2d902918012
Ronald <lifj@ruone.cn>
2025-02-27 21:01:41 +0800

    Initial commit: 初始化TypeScript项目

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // output: 'standalone',
+    // productionBrowserSourceMaps: true,
+};
 
 module.exports = nextConfig;
